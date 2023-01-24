3acae15e82abe8c36dddf9911ffe4172973c65f5
Liz Martins <elizabeth.martins5@gmail.com>
2023-01-23 21:10:31 -0500

    made classes, created checkMatch

--- script.js
+++ script.js
@@ -2,17 +2,9 @@ const gameBoard = document.querySelectorAll('board')
 // const playerOne =
 // const playerTwo =
 // const currentPlayer = playerOne
-// const scoreBoard = document.querySelector()
 const startButton = document.getElementById('play')
-// let score = document.querySelector()
 
 // const resetButton = document.querySelector('.reset')
-
-//////////////////////////////////////////////////////
-
-// const startGame = () => {
-//   document.body.classList.toggle('pointer event')
-// }
 
 ///////////function PlayerTurn/////////////////////
 
@@ -20,17 +12,59 @@ const startButton = document.getElementById('play')
 const card = document.querySelectorAll('.card')
 
 flipCard = (e) => {
-  e.target.classList.toggle('visible')
-  // setTimeout(() => e.target.classList.toggle('visible'), 1000)
-  console.log('hello!')
+  // setTimeout(() => e.target.classList.toggle('invisible'))
+  const banana = e.target.getAttribute('banana')
+  e.target.classList.toggle(banana)
+  console.log('hello!' + banana)
+  checkMatch(banana)
 }
 
 card.forEach((card) => {
   card.addEventListener('click', flipCard)
 })
 
-console.log('visible')
+////////////////////////////function checkMatch///////////////////////////////////////
+//
+let choiceOne = ''
+let choiceTwo = ''
 
+const checkMatch = (apple) => {
+  if (choiceOne.length > 0) {
+    choiceTwo = apple
+    if (choiceOne === choiceTwo) {
+      // win conditions
+      console.log('win')
+      choiceOne = ''
+      choiceTwo = ''
+    } else {
+      console.log('no match')
+      // write a query selector or get elements by class and pass in ChoiceOne[0] and remove anything w/ class "ladybug/choiceOne" and toggle it off, same for choiceTwo, that will flip them back over
+      choiceOne = ''
+      choiceTwo = ''
+    }
+  } else {
+    choiceOne = apple
+  }
+}
+
+// let choiceOne = banana
+// let choiceTwo = ''
+// if choiceOne.length > 0
+// if choiceTwo.length >0
+// find id and give it an ! and it will supersede
 /////////////function Update Score, when player makes a match the scoreboard updates////////////////////
+let scoreBoard = document.querySelector('.scoreboard')
+let score = 0
+// let image = document.querySelector(img.src)
+
+// upDateScore = () => {
+//   if ( === ) {
+//     for (let i = 0; i < matches.length; i++) {
+//       score = score += 2
+//     }
+//     scoreBoard.innerHTML = `${score}`
+//   }
+// }
+// console.log('This works' + upDateScore())
 
 /////////////function play again resets board////////////////////
